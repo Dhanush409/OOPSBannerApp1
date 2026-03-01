eb1cced30f875dfce27ec16eb85f3108fd705fa6
Dhanush409 <dp8916@srmist.edu.in>
2026-03-01 22:32:13 +0530

    Update OOPSbannerapp.java

--- OOPSbannerapp.java
+++ OOPSbannerapp.java
@@ -1,17 +1,26 @@
-public class OOPSBannerApp {
+public class stringjoin{
 
     public static void main(String[] args) {
-        System.out.println("   **   " + "   " + "   **   " + "   " + " **********   " + "   *********");
-        System.out.println(" **  ** " + "   " + " **  ** " + "   " + " **      **   " + " **         ");
-        System.out.println("*      *" + "   " + "*      *" + "   " + " **      **   " + "*           ");
-        System.out.println("*      *" + "   " + "*      *" + "   " + " **      **   " + "*           ");
-        System.out.println("*      *" + "   " + "*      *" + "   " + " **********   " + " ********   ");
-        System.out.println("*      *" + "   " + "*      *" + "   " + " **********   " + "         ** ");
-        System.out.println("*      *" + "   " + "*      *" + "   " + " **           " + "           *");
-        System.out.println("*      *" + "   " + "*      *" + "   " + " **           " + "           *");
-        System.out.println(" **  ** " + "   " + " **  ** " + "   " + " **           " + "         ** ");
-        System.out.println("   **   " + "   " + "   **   " + "   " + " **           " + "*********   ");
+        renderBanner();
     }
 
+    public static void renderBanner() {
+
+        String line1 = String.join(" ", "*****",   "*****",  "****",  "*****");
+
+        String line2 = String.join(" ", "*   *",   "*   *",  "*  *",  "*    ");
+
+        String line3 = String.join(" ", "*   *",   "*   *",  "****",  "*****");
+
+        String line4 = String.join(" ", "*   *",   "*   *",  "*   ",  "    *");
+
+        String line5 = String.join(" ", "*****",   "*****",  "*   ",  "*****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
 }
-  
+
